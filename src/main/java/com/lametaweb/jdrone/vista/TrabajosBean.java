ac4c181fd11dd21a9f435ecd31cfeea76fa001d7
La metaweb <correodelametaweb@gmail.com>
2015-07-29 09:30:40 +0200

    creacion backing bean para pantalla gestión trabajos

---
+++ src/main/java/com/lametaweb/jdrone/vista/TrabajosBean.java
@@ -0,0 +1,14 @@
+package com.lametaweb.jdrone.vista;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class TrabajosBean {
+
+	public TrabajosBean() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
