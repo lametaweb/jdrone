2c6fc9af682701041bae40c19273eaea6aa8a4f1
La metaweb <correodelametaweb@gmail.com>
2015-08-01 21:08:20 +0200

    cambio scope. implementación borrado trabajo.

--- src/main/java/com/lametaweb/jdrone/vista/TrabajosBean.java
+++ src/main/java/com/lametaweb/jdrone/vista/TrabajosBean.java
@@ -1,14 +1,43 @@
 package com.lametaweb.jdrone.vista;
 
-import javax.enterprise.context.RequestScoped;
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.lametaweb.jdrone.negocio.TrabajoFacade;
+import com.lametaweb.jdrone.persistencia.Trabajo;
+
 @Named
-@RequestScoped
-public class TrabajosBean {
+@ViewScoped
+public class TrabajosBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private List<Trabajo> trabajos;
+	@Inject
+	private TrabajoFacade trabajoFacade;
 
 	public TrabajosBean() {
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	public String eliminar(Trabajo trabajo){
+		trabajoFacade.remove(trabajo);
+		actualizaModeloTrabajos();
+		return "";
+	}
+    
+	public void actualizaModeloTrabajos(){
+		trabajos = trabajoFacade.findAll();
+	}
+	
+	
+	public List<Trabajo> getTrabajos() {
+		return trabajos;
+	}
 }
