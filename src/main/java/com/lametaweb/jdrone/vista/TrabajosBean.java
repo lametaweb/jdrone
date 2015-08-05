8fa013ab6b819fbceb0108dc20f7c58bf33ed8b6
La metaweb <correodelametaweb@gmail.com>
2015-08-06 00:48:40 +0200

    eliminación imports

--- src/main/java/com/lametaweb/jdrone/vista/TrabajosBean.java
+++ src/main/java/com/lametaweb/jdrone/vista/TrabajosBean.java
@@ -3,8 +3,6 @@ package com.lametaweb.jdrone.vista;
 import java.io.Serializable;
 import java.util.List;
 
-import javax.faces.application.FacesMessage;
-import javax.faces.context.FacesContext;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
